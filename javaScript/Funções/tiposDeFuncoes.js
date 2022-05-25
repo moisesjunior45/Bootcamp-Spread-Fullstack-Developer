84e45ea9882717200ee2b81d337c4d74acb17393
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-05-25 14:01:28 -0300

    Create tiposDeFuncoes.js

---
+++ javaScript/Funções/tiposDeFuncoes.js
@@ -0,0 +1,36 @@
+// Função Anônima
+
+const soma = function(a,b) {
+    return a + b;
+}
+console.log(soma(2,4))
+
+// Função autoinvocável
+
+const soma2 = (
+    function(a,b) {
+        return a + b;
+}
+)(1,2);
+
+console.log(soma2);
+
+// Função Callbacks - Função que é passada como argumento para outra função
+
+const calc = function(operacao, num1, num2) {
+    return operacao(num1, num2);
+}
+
+const soma3 = function(num1, num2) {
+    return num1 + num2;
+}
+
+const sub = function(num1, num2) {
+    return num1 - num2;
+}
+
+const resultSoma = calc(soma, 1, 2);
+const resultSub = calc(sub, 1, 2);
+
+console.log(resultSoma);
+console.log(resultSub);
