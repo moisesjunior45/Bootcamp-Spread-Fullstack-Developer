48cf445961c9d001df98b5c5156f3a3b9ccd2172
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-07-11 11:01:50 -0300

    Estudos 11/07

---
+++ javaScript/Modulos/Atividade-pratica/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Moisés", 19));
+console.log(mostraCidade("João", "Recife"));
+console.log(mostraHobby("Rafael","Jogar video game"));
